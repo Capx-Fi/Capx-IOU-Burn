44e388b4d4b4b2d0d12086ba90d772b417cbdade
Madhur Dixit <dmadhur6@gmail.com>
2023-08-23 17:13:44 +0530

    Migration test update

--- contracts/Eater.sol
+++ contracts/Eater.sol
@@ -6,77 +6,56 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol"; // Import SafeERC20 library
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; // Import IERC20 interface
 
-contract Eater is Pausable, Ownable {
+contract ClaimCapx is Pausable, Ownable {
     using SafeERC20 for IERC20; // Use SafeERC20 library
 
-    mapping (address=>uint256) public tokenBalances;
-    mapping (address=>address) public iouMap;
-    mapping (address=>mapping (address=>uint256)) claimMap;
+    IERC20 public CAPX;
+	IERC20 public xCAPX;
 
+    mapping (IERC20=>uint256) public tokenBalances;
+    mapping (address=>uint256) public claimMap;
 
-    // Safe transfer of ERC20 tokens using safeERC20
-    function _safeTransferERC20(
-        address token,
-        address to,
-        uint256 amount
-    ) internal {
-        IERC20(token).safeTransfer(to, amount);
-    }
-
-    // Safe transferFrom of ERC20 tokens using safeERC20
-    function _safeTransferFromERC20(
-        address token,
-        address from,
-        address to,
-        uint256 amount
-    ) internal {
-        IERC20(token).safeTransferFrom(from, to, amount);
-    }
+    constructor(address _CAPXAddress, address _xCAPXAddress) {
+		CAPX = IERC20(_CAPXAddress);
+		xCAPX = IERC20(_xCAPXAddress);
+	}
 
     // Deposit ERC20 tokens into the contract
-    function depositToken(address token, uint256 amount) external onlyOwner {
-        require(token != address(0), "Invalid token address");
+    function depositToken(IERC20 token, uint256 amount) external onlyOwner {
+        require(token == CAPX, "Should Be capx token");
         require(amount > 0, "Amount must be greater than 0");
 
         tokenBalances[token] += amount;
-        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+        token.safeTransferFrom(msg.sender, address(this), amount);
     }
 
-    function updateIouMap(address iouAddress, address tokenAddress) external onlyOwner {
-        
-        require(iouAddress != address(0) && tokenAddress != address(0), "Invalid token address");
 
-        iouMap[tokenAddress] = iouAddress;
-        
+    function _burnIOU(IERC20 iouToken, uint256 amount) internal {
+        require(iouToken==xCAPX,"Not a valid CAPX IOU Token");
+        claimMap[msg.sender] += amount;
+       iouToken.safeTransferFrom(msg.sender, address(this), amount);
     }
 
-    function _burnIOU(address iouToken, uint256 amount) internal {
-        require(iouMap[iouToken]!=address(0),"Not a valid IOU Token");
-        claimMap[msg.sender][iouToken] += amount;
-        _safeTransferFromERC20(iouMap[iouToken], msg.sender, address(this), amount);
+    function _claimIOU(IERC20 iouToken, uint256 amount) internal {
+         require(iouToken==xCAPX,"Not a valid CAPX IOU Token");
+        claimMap[msg.sender] -= amount;
+        tokenBalances[CAPX] -= amount;
+        CAPX.safeTransfer(msg.sender, amount);
     }
 
-    function _claimIOU(address iouToken, uint256 amount) internal {
-        require(iouMap[iouToken]!=address(0),"Not a valid IOU Token");
-        claimMap[msg.sender][iouToken] -= amount;
-        address actualToken = iouMap[iouToken];
-        tokenBalances[actualToken] -= amount;
-        _safeTransferERC20(actualToken, msg.sender, amount);
-    }
-
-    function burnIOU(address iouToken, uint256 amount) external {
+    function burnIOU(IERC20 iouToken, uint256 amount) external {
 
         _burnIOU(iouToken, amount);
         
     }
 
-    function claimIOU(address iouToken, uint256 amount) external {
+    function claimIOU(IERC20 iouToken, uint256 amount) external {
 
         _claimIOU(iouToken, amount);
         
     }
 
-    function burnAndClaim(address iouToken, uint256 amount) external {
+    function burnAndClaim(IERC20 iouToken, uint256 amount) external {
         _burnIOU(iouToken, amount);
         _claimIOU(iouToken, amount);
     }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,35 +1,71 @@
-// SPDX-License-Identifier: MIT
 const TestToken = artifacts.require("TestToken");
-const Eater = artifacts.require("Eater");
+const ClaimCapx = artifacts.require("ClaimCapx");
 
-module.exports = async function (deployer, network, accounts) {
-  // Deploy TestToken contract
-  await deployer.deploy(TestToken);
-  const testTokenInstance = await TestToken.deployed();
+module.exports = async function(deployer, network, accounts) {
+    // Deploy CAPX Token
+    await deployer.deploy(TestToken);
+    const capxToken = await TestToken.deployed();
+    console.log("CAPX Token deployed at:", capxToken.address);
 
-  // Deploy Eater contract
-  await deployer.deploy(Eater);
-  const eaterInstance = await Eater.deployed();
+    // Deploy xCAPX Token
+    await deployer.deploy(TestToken);
+    const xcapxToken = await TestToken.deployed();
+    console.log("xCAPX Token deployed at:", xcapxToken.address);
 
-// Example 5: Deposit TestToken into Eater contract and log balance change
-const depositAmount = web3.utils.toWei("1000", "ether"); // Amount in wei
-const ownerBalanceBefore = await testTokenInstance.balanceOf(accounts[0]);
-const contractBalanceBefore = await testTokenInstance.balanceOf(eaterInstance.address);
-const contractMappingBalanceBefore = await eaterInstance.tokenBalances(testTokenInstance.address);
+    // Deploy ClaimCapx contract with addresses of CAPX and xCAPX tokens
+    await deployer.deploy(ClaimCapx, capxToken.address, xcapxToken.address);
+    const claimCapx = await ClaimCapx.deployed();
+    console.log("ClaimCapx contract deployed at:", claimCapx.address);
 
-await testTokenInstance.approve(eaterInstance.address, depositAmount, { from: accounts[0] });
-await eaterInstance.depositToken(testTokenInstance.address, depositAmount);
+    // Deposit CAPX tokens into ClaimCapx contract
+    const depositAmount = web3.utils.toWei("1000", "ether");
+    await capxToken.approve(claimCapx.address, depositAmount);
+    await claimCapx.depositToken(capxToken.address, depositAmount);
 
-const ownerBalanceAfter = await testTokenInstance.balanceOf(accounts[0]);
-const contractBalanceAfter = await testTokenInstance.balanceOf(eaterInstance.address);
-const contractMappingBalanceAfter = await eaterInstance.tokenBalances(testTokenInstance.address);
+    let contractBalance = await capxToken.balanceOf(claimCapx.address);
+    if (contractBalance.toString() === depositAmount) {
+        console.log("Successfully deposited 1000 CAPX into ClaimCapx contract!");
+    } else {
+        console.error("Failed to deposit CAPX into ClaimCapx contract.");
+    }
 
-console.log(`Owner balance before: ${ownerBalanceBefore.toString()}`);
-console.log(`Owner balance after: ${ownerBalanceAfter.toString()}`);
-console.log(`Contract balance before: ${contractBalanceBefore.toString()}`);
-console.log(`Contract balance after: ${contractBalanceAfter.toString()}`);
-console.log(`Contract mapping balance before: ${contractMappingBalanceBefore.toString()}`);
-console.log(`Contract mapping balance after: ${contractMappingBalanceAfter.toString()}`);
-console.log(`Deposited ${depositAmount} TestToken into Eater contract.`);
+    // Burn xCAPX IOU tokens
+    const burnAmount = web3.utils.toWei("500", "ether");
+    await xcapxToken.approve(claimCapx.address, burnAmount, {from: accounts[0]});
+    await claimCapx.burnIOU(xcapxToken.address, burnAmount);
 
+    let userClaimAmount = await claimCapx.claimMap(accounts[0]);
+    if (userClaimAmount.toString() === burnAmount) {
+        console.log("Successfully burned 500 xCAPX IOU tokens!");
+    } else {
+        console.error("Failed to burn xCAPX IOU tokens.");
+    }
+
+    // Claim CAPX tokens using xCAPX IOU tokens
+    const claimAmount = web3.utils.toWei("200", "ether");
+    await xcapxToken.approve(claimCapx.address, claimAmount, {from: accounts[0]});
+    await claimCapx.claimIOU(xcapxToken.address, claimAmount);
+
+    contractBalance = await capxToken.balanceOf(claimCapx.address);
+    userClaimAmount = await claimCapx.claimMap(accounts[0]);
+    if (contractBalance.toString() === web3.utils.toWei("800", "ether") && userClaimAmount.toString() === web3.utils.toWei("300", "ether")) {
+        console.log("Successfully claimed 200 CAPX using xCAPX IOU tokens!");
+    } else {
+        console.error("Failed to claim CAPX using xCAPX IOU tokens.");
+    }
+
+
+    // Burn and claim in a single transaction
+    const amount = web3.utils.toWei("100", "ether");
+    await xcapxToken.approve(claimCapx.address, amount, {from: accounts[0]});
+    await claimCapx.burnAndClaim(xcapxToken.address, amount);
+
+    contractBalance = await capxToken.balanceOf(claimCapx.address);
+    userClaimAmount = await claimCapx.claimMap(accounts[0]);
+
+    if (contractBalance.toString() === web3.utils.toWei("700", "ether") && userClaimAmount.toString() === web3.utils.toWei("300", "ether")) {
+        console.log("Successfully burned and claimed 100 CAPX in a single transaction!");
+    } else {
+        console.error("Failed to burn and claim CAPX in a single transaction.");
+    }
 };
